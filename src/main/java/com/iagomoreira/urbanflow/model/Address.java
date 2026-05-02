3f921a6f0b6325e1dd197963dacccb5be19538d1
Iago Moreira <iagomoreira2353@gmail.com>
2026-05-02 16:30:37 -0300

    feat: Adjusting the model classes for better readability

--- src/main/java/com/iagomoreira/urbanflow/model/Address.java
+++ src/main/java/com/iagomoreira/urbanflow/model/Address.java
@@ -2,15 +2,9 @@ package com.iagomoreira.urbanflow.model;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Document
 public class Address implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -23,4 +17,74 @@ public class Address implements Serializable {
 	private String state;
 	private String complement;
 
+	public Address() {
+	}
+
+	public Address(String cep, String street, String number, String neighborhood, String city, String state,
+			String complement) {
+		super();
+		this.cep = cep;
+		this.street = street;
+		this.number = number;
+		this.neighborhood = neighborhood;
+		this.city = city;
+		this.state = state;
+		this.complement = complement;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getComplement() {
+		return complement;
+	}
+
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
 }
--- src/main/java/com/iagomoreira/urbanflow/model/User.java
+++ src/main/java/com/iagomoreira/urbanflow/model/User.java
@@ -7,20 +7,7 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Document(collection = "users")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@EqualsAndHashCode(of = "id")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -37,4 +24,100 @@ public class User implements Serializable {
 	private LocalDateTime createdAt;
 	private LocalDateTime updateAt;
 
+	public User() {
+	}
+
+	public User(String id, String name, String email, String password, String cpf, Address address,
+			LocalDateTime createdAt, LocalDateTime updateAt) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.cpf = cpf;
+		this.address = address;
+		this.createdAt = createdAt;
+		this.updateAt = updateAt;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
+	}
+
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
 }
